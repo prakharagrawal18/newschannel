f8ac1d12ad9157701c5c5f5665fa425c27235379
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-04-20 10:04:25 +0530

    set up configuration file.

---
+++ includes/config.php
@@ -0,0 +1,29 @@
+<?php
+
+    /**
+     * Set the details according to the environment in which the
+     * application is being used.
+     * 
+     * @var array
+     */
+    return [
+        'development' => [
+            'site_title' => 'News Channel',
+            'database'   => [
+                'host'     => '',
+                'name'     => '',
+                'password' => '',
+                'username' => ''
+            ]
+        ],
+
+        'production' => [
+            'site_title' => 'News Channel',
+            'database'   => [
+                'host'     => 'localhost',
+                'name'     => 'newschannel',
+                'password' => '',
+                'username' => 'root'
+            ]
+        ]
+    ];
---
+++ includes/init.php
@@ -0,0 +1,9 @@
+<?php
+
+    /**
+     * Config file should be the first to include since this will be used
+     * in the complete application for fetching the environment
+     * configuration.
+     */
+    require_once( __DIR__ . '/core/config.php');
+    require_once( __DIR__ . '/core/function.php');
